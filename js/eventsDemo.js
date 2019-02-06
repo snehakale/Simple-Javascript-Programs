ee268c5a76955252919013f9449ae295da808a04
Sneha Kale <kalesneha88@gmail.com>
2019-02-06 11:46:01 -0500

    docs: Update files with new code for event handling
    Update files viz. eventsDemo.html and eventsDemo.js with new code for event handling program.

--- js/eventsDemo.js
+++ js/eventsDemo.js
@@ -9,17 +9,27 @@ function showSelected() {
     document.getElementById("span2").innerHTML = "Favorite pet animal = " + val;
 }
 
-// functions to call for onmuseover and onmouseout events
+// functions to call for onmuseover event
 function mouseOver() {
     document.getElementById("span3").style.color = "#c17639";
 }
 
+// function to call for onmouseout event
 function mouseOut() {
     document.getElementById("span3").style.color = "black";
 }
 
-
 //function to call for onkeypress event
 function showKeyPrssed() {
    document.getElementById("span4").innerHTML = "Key has been presed in the input box!!";
+}
+
+// function to call for onfocus event
+function showOnFocusColor() {
+    document.getElementById("demo2").style.background = "#42e5f4";
+}
+
+// function to call for onblur event
+function showOnBlurColor() {
+    document.getElementById("demo3").style.background = "#f438d2";
 }
